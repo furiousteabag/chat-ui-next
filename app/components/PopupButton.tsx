4f85f01d7dc61a5dc739168e4b3cbc3c900cfd5f
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-22 17:11:40 +0530

    add button with unread dot and popup msg

--- app/components/PopupButton.tsx
+++ app/components/PopupButton.tsx
@@ -14,12 +14,12 @@ export default function PopupButton({
   askguruAPI: AskguruApi
 }) {
   const [isCollapsed, setIsCollapsed] = useState(true)
-  const [hasInteracted, setHasInteracted] = useState(false)
+  const [hasInteracted, setHasInteracted] = useState(true)
 
   useEffect(() => {
     askguruAPI.logEvent({ eventType: "POPUP_SEEN" })
-    if (localStorage.getItem("askguru-has-interacted")) {
-      setHasInteracted(true)
+    if (!localStorage.getItem("askguru-has-interacted")) {
+      setHasInteracted(false)
     }
   }, [])
 
@@ -27,20 +27,23 @@ export default function PopupButton({
     setIsCollapsed(!isCollapsed)
     setHasInteracted(true)
     localStorage.setItem("askguru-has-interacted", "true")
+    askguruAPI.logEvent({ eventType: "POPUP_CALLED" })
   }
-
-  if (!configuration.token) return <h1>PNH</h1>
 
   console.log(configuration)
   console.log(askguruConfiguration)
 
   return (
-    <>
-      {!configuration.token}
-      <div className={styles.wrapper}></div>
-      <Image alt="kek" src="/original-chevron.svg" width={150} height={150} />
-      <Image alt="kek" src="/next.svg" width={150} height={150} />
-      <Image alt="lol" src="/original-chat-icon.svg" width={150} height={150} />
-    </>
+    <button
+      className={styles.button}
+      style={{ backgroundColor: "#" + configuration.color }}
+      onClick={() => handleClick()}
+    >
+      <Image alt="" src={isCollapsed ? configuration.popupIcon : "/chevron.svg"} width={64} height={64} />
+      {configuration.addUnreadDot && !hasInteracted && <div className={styles.unreadDot} />}
+      {configuration.popupMessage && !hasInteracted && (
+        <div className={styles.popupWidget} dangerouslySetInnerHTML={{ __html: configuration.popupMessage }} />
+      )}
+    </button>
   )
 }
