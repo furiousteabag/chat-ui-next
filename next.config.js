a42952689c102b84b84f41d461b1d5c8f5947977
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-22 17:08:44 +0530

    allow popup images from 3rd party link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "i.ibb.co",
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
