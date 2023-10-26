4e056d7a412f4329236b34a91212c240af69b6df
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-26 10:10:56 +0530

    upd default configuration

--- app/configuration.ts
+++ app/configuration.ts
@@ -3,14 +3,13 @@ import { AskguruConfiguration, Configuration } from "@/app/_interfaces"
 export const defaultConfiguration: Configuration = {
   token: "",
   color: "15BE6C",
-  // popupIcon: "https://i.ibb.co/Smdd1Xc/clickhelp-logo.png",
   popupIcon: "/images/popup/askguru.svg",
   popupMessage: "Try <b>AI-powered</b> search!",
   addUnreadDot: true,
   whitelabel: false,
   lang: "en-US",
   windowHeading: "Chat with AI Assistant",
-  welcomeMessage: "Hi, ask me anything",
+  welcomeMessage: "👋 Hi! Ask me anything...",
   bottomIndent: 24,
   rightIndent: 24,
 }
