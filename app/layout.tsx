7c4c0dd6047f7ae4e7fd72574459f87b8c3a9885
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-26 14:02:32 +0530

    do not zoom in on apple devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Chat Pop-Up",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
